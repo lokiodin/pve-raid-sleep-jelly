5b0505e511b0fed0a073d727de926a801f1250f5
Romain <49552659+lokiodin@users.noreply.github.com>
2022-05-29 13:15:05 +0200

    Add v1.2

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -o nounset -o pipefail -o errexit
 install -m 755 -v pve-raid-sleep-jelly /usr/local/sbin
 install -m 644 -v pve-raid-sleep-jelly.service /etc/systemd/system
 
-test -f /etc/pve-raid-sleep-jelly.conf || install -m 644 -v pve-raid-sleep-jelly.conf /etc/pve-raid-sleep-jelly.conf
+test -f /etc/pve-raid-sleep-jelly/pve-raid-sleep-jelly.conf || install -m 644 -v pve-raid-sleep-jelly.conf /etc/pve-raid-sleep-jelly/pve-raid-sleep-jelly.conf
 
 systemctl daemon-reload
 systemctl enable pve-raid-sleep-jelly.service
